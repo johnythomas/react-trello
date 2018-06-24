1d1caec99949da7a2ef2cdbd1c6e52e55e774350
Johny Thomas <johnythomas007@gmail.com>
2018-06-24 16:51:30 +0530

    Feat: Add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react"
+import { Route, Switch } from "react-router-dom"
 import ListBoards from "./ListBoards"
+import Board from "./Board"
 import "./App.css"
 
 const App = () => (
@@ -7,7 +9,10 @@ const App = () => (
     <div className="title-container">
       <h1 className="title">React Trello</h1>
     </div>
-    <ListBoards />
+    <Switch>
+      <Route exact path="/" component={ListBoards} />
+      <Route exact path="/board" component={Board} />
+    </Switch>
   </div>
 )
 
