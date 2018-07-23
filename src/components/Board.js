930f410dbf971efa561efe681ffb87d3725106da
Johny Thomas <johnythomas007@gmail.com>
2018-07-23 22:41:33 +0530

    Feat: Show fetched board details and show in UI

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,42 +1,54 @@
 import React from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
 import "./Board.css"
 
-const Board = () => (
+const Board = ({ board, lists, items }) => (
   <div className="board">
-    <div className="list">
-      <h3 className="list-heading">List Name</h3>
-      <div className="list-item">
-        <span className="list-item-text">list item text</span>
+    <div>{board.name}</div>
+    {lists.map(list => (
+      <div className="list" key={list.id}>
+        <h3 className="list-heading">{list.name}</h3>
+        {list.items.map(itemId => (
+          <div className="list-item" key={itemId}>
+            <span className="list-item-text">{items[itemId].body}</span>
+          </div>
+        ))}
       </div>
-      <div className="list-item">
-        <span className="list-item-text">list item text</span>
-      </div>
-      <div className="list-item">
-        <span className="list-item-text">list item text</span>
-      </div>
-      <div className="list-item">
-        <span className="list-item-text">list item text</span>
-      </div>
-      <div className="list-item">
-        <span className="list-item-text">list item text</span>
-      </div>
-      <div className="list-item">
-        <span className="list-item-text">list item text</span>
-      </div>
-    </div>
-    <div className="list">
-      <h3>List Name</h3>
-      <div className="list-item">
-        <span className="list-item-text">list item text</span>
-      </div>
-      <div className="list-item">
-        <span className="list-item-text">list item text</span>
-      </div>
-    </div>
+    ))}
     <div className="add-list">
       <span className="add-list-text">Add a list...</span>
     </div>
   </div>
 )
 
-export default Board
+Board.propTypes = {
+  board: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }).isRequired,
+  lists: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  items: PropTypes.shape({
+    id: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      body: PropTypes.string.isRequired
+    })
+  }).isRequired
+}
+
+const mapStateToProps = (state, props) => {
+  const board = state.boards[props.match.params.id]
+  const { lists, items } = state
+  const boardLists = board.lists ? board.lists.map(listId => lists[listId]) : []
+  return {
+    board,
+    lists: boardLists,
+    items
+  }
+}
+
+export default connect(mapStateToProps)(Board)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux"
 import boards from "./boards"
-import list from "./list"
-import item from "./item"
+import lists from "./list"
+import items from "./item"
 
 export default combineReducers({
   boards,
-  list,
-  item
+  lists,
+  items
 })
