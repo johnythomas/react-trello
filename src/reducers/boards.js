f8725a0d394412eff7c33bcfa7cabd2aefcbb378
Johny Thomas <johnythomas007@gmail.com>
2018-07-26 23:11:10 +0530

    Fix: Fix boards state being cleared after reload of board page

--- src/reducers/boards.js
+++ src/reducers/boards.js
@@ -3,9 +3,14 @@ import { BOARDS_FETCHED, BOARD_FETCHED } from "../actions/boards"
 const boards = (state = {}, action) => {
   switch (action.type) {
     case BOARDS_FETCHED:
-      return {
-        ...action.boards
-      }
+      if (!action.boards) return state
+      return Object.keys(action.boards).reduce(
+        (acc, boardId) => ({
+          ...acc,
+          [boardId]: state[boardId] ? state[boardId] : action.boards[boardId]
+        }),
+        {}
+      )
     case BOARD_FETCHED:
       return {
         ...state,
